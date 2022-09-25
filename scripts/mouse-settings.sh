2f7ffaea4e11b0ac8493ef9dd603d824e53b5c1b
Eirik Alvheim <eirikalv@icloud.com>
2022-09-25 13:06:23 +0200

    Added theme files

---
+++ scripts/mouse-settings.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+xinput --set-prop 'pointer:''Kingston Technology Company HyperX Pulsefire FPS Pro' 'libinput Accel Profile Enabled' 1, 0
+xinput --set-prop 'pointer:''Kingston Technology Company HyperX Pulsefire FPS Pro' 'libinput Accel Speed' -0.3
